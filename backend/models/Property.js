69b16acc12fcc804f22511838cddc1aebb3d51b2
Shawn Stewart <s.stewart0220@gmail.com>
2019-01-19 16:41:32 -0500

    Created an array with some property data.

---
+++ backend/models/Property.js
@@ -0,0 +1,146 @@
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+// const ObjectId = Schema.Types.ObjectId;
+
+// const PropertySchema = new Schema();
+
+// module.exports = Property = mongoose.model("properties", PropertySchema);
+
+module.exports = [
+  {
+    owned: false,
+    name: "Mediterranean Avenue",
+    cost: 60,
+    mortgage: 30,
+    building: 50,
+    houses: 0,
+    hotels: 0,
+    rent: {
+      0: 2,
+      1: 10,
+      2: 30,
+      3: 90,
+      4: 160,
+      5: 250
+    }
+  },
+  {
+    owned: false,
+    name: "Baltic Avenue",
+    cost: 60,
+    mortgage: 30,
+    building: 50,
+    houses: 0,
+    hotels: 0,
+    rent: {
+      0: 4,
+      1: 20,
+      2: 60,
+      3: 180,
+      4: 320,
+      5: 450
+    }
+  },
+  {
+    owned: false,
+    name: "Oriental Avenue",
+    cost: 100,
+    mortgage: 50,
+    building: 50,
+    houses: 0,
+    hotels: 0,
+    rent: {
+      0: 6,
+      1: 30,
+      2: 90,
+      3: 270,
+      4: 400,
+      5: 550
+    }
+  },
+  {
+    owned: false,
+    name: "Vermont Avenue",
+    cost: 100,
+    mortgage: 50,
+    building: 50,
+    houses: 0,
+    hotels: 0,
+    rent: {
+      0: 6,
+      1: 30,
+      2: 90,
+      3: 270,
+      4: 400,
+      5: 550
+    }
+  },
+  {
+    owned: false,
+    name: "Connecticut Avenue",
+    cost: 120,
+    mortgage: 60,
+    building: 50,
+    houses: 0,
+    hotels: 0,
+    rent: {
+      0: 8,
+      1: 40,
+      2: 100,
+      3: 300,
+      4: 450,
+      5: 600
+    }
+  },
+  {
+    owned: false,
+    name: "St. Charles Place",
+    cost: 140,
+    mortgage: 70,
+    building: 100,
+    houses: 0,
+    hotels: 0,
+    rent: {
+      0: 10,
+      1: 50,
+      2: 150,
+      3: 450,
+      4: 625,
+      5: 750
+    }
+  },
+  {
+    owned: false,
+    name: "States Avenue",
+    cost: 140,
+    mortgage: 70,
+    building: 100,
+    houses: 0,
+    hotels: 0,
+    rent: {
+      0: 10,
+      1: 50,
+      2: 150,
+      3: 450,
+      4: 625,
+      5: 750
+    }
+  },
+  {
+    owned: false,
+    name: "Virginia Avenue",
+    cost: 160,
+    mortgage: 80,
+    building: 100,
+    houses: 0,
+    hotels: 0,
+    rent: {
+      0: 12,
+      1: 60,
+      2: 180,
+      3: 500,
+      4: 700,
+      5: 900
+    }
+  }
+];
